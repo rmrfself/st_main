9587dd4f61abfcbde6cc2a15d59a81616bc19eff
MikeZhang <154245916@qq.com>
2018-11-28 11:09:39 +0800

    remove footer and rebuild model

--- extra-addons/emb_portal/__manifest__.py
+++ extra-addons/emb_portal/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website','emb_base'],
+    'depends': ['website'],
 
     # always loaded
     'data': [
