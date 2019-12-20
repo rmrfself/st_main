47f53360a6da49064b76c0832e2b4d12f30acbf8
MikeZhang <154245916@qq.com>
2019-12-21 03:26:32 +0800

    readme

--- extra-addons/emb_portal/models/product.py
+++ extra-addons/emb_portal/models/product.py
@@ -10,7 +10,6 @@ import json
 
 class ProductGarmentBrand(models.Model):
     _name = "product.garment.brand"
-
     name = fields.Char(string="Brand", required=True)
 
 # contains 6 positions: t,b,f,b,l,r
@@ -52,10 +51,11 @@ class MrpBomLine(models.Model):
 class Product(models.Model):
     _inherit = "product.product"
 
-    garment_id = fields.Many2one('sale_order_garment', string='Garment Reference', required=False, ondelete='cascade', index=False, copy=True)
-
-    logo_id = fields.Many2one('sale_order_logo', string='Logo Reference', required=False, ondelete='cascade', index=False, copy=True)
+    product_type = fields.Char('Type', required=True)
     
+    garment_id = fields.Many2one('sale_order_garment', string='Garment Reference', required=False, ondelete='cascade')
+
+    logo_id = fields.Many2one('sale_order_logo', string='Logo Reference', required=False, ondelete='cascade')
 
 class GarmentInfo(models.Model):
     _name = "product.garment.info"
@@ -96,67 +96,58 @@ class DOrderConfirm(models.Model):
 
     status = fields.Boolean(default=False)
 
+class SaleOrderGarmentInfo(models.Model):
+    _name = 'sale.order.garment.info'
+    _description = 'Sales Order Garment List'
+    _order = 'id' 
+
+    sequence = fields.Integer(string='Sequence', help="Gives the sequence order when displaying a list of analytic distribution")
+
+    order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)
+    garment_id = fields.Many2one('product.garment', string='Garment Reference', required=True)
+    name = fields.Char(string='Description', required=True)
+    style = fields.Char(string='Style')
+    brand = fields.Char(string='Brand')
+    color = fields.Char(string='Color')
+    qty = fields.Integer(string='Quantity')
+    qty_data = fields.Char(string='Quantity Detail')
+
 class SaleOrderGarment(models.Model):
     _name = 'sale.order.garment'
     _description = 'Sales Order Garment List'
-    _order = 'order_id, id' 
+    _order = 'id' 
 
-    order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)   
     garment_id = fields.Many2one('product.garment', string='Garment Reference', required=True)
-
-    logo_ids = fields.One2many('sale.order.logo', 'sale_order_garment_id', string='Order Logos', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True, auto_join=True)
-
-    image = fields.Binary('Image', attachment=True)
-    design_image = fields.Binary('Image', attachment=True)
-
-    name = fields.Text(string='Description', required=True)
-    sequence = fields.Integer(string='Sequence', default=10)
-
-    garment_face = fields.Text(string='Garment Face')
-    garment_type = fields.Text(string='Garment Type')
-    garment_style = fields.Text(string='Garment Style')
-    garment_brand = fields.Text(string='Garment Brand')
-    garment_color = fields.Text(string='Garment Color')
-    garment_size = fields.Text(string='Garment Size')
-    garment_qty = fields.Integer(string='Garment Qty')
-    garment_location = fields.Text(string='Logo Location')
-    garment_designs = fields.Text(string='Logo Designs')
+    product_type = fields.Char(string='Type')
+    name = fields.Char(string='Description', required=True)
+    style = fields.Char(string='Style')
+    brand = fields.Char(string='Brand')
+    color = fields.Char(string='Color')
+    face = fields.Char(string='Face')
+    image_id = fields.Integer(string='Image Id')
+    location = fields.Char(string='Location')
+    line_info = fields.Char(string='Logo Colors')
 
 class SaleOrderLogo(models.Model):
     _name = 'sale.order.logo'
     _description = 'Sales Order Logo List'
-    _order = 'order_id, id' 
-
-    order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)   
-
-    sale_order_garment_id = fields.Many2one('sale.order.garment', string='Garment', required=True, ondelete='cascade', index=True, copy=False)
+    _order = 'id' 
     
-    image = fields.Binary('Image', attachment=True)
-    
-    name = fields.Char(string='Description', required=True)
-    sequence = fields.Integer(string='Sequence', default=10)
-
-    surcharge = fields.Float(string='Surcharge')
-    surcharge_description = fields.Char(string='Surcharge Desc')
-    service_type = fields.Char(string='Serice Type')
-    service_name = fields.Char(string='Service Name')
-
+    name = fields.Char(string='Name', required=True)
+    service = fields.Char(string='Service Name')
     line_data = fields.Char(string='Line Data')
-
     price = fields.Float(string='Price')
     discount = fields.Float(string='Discount')
-    location = fields.Char(string='Location')
-    garment_qty = fields.Integer(string='Quantity')
-
-    position_left = fields.Float(string='Position left')
-    position_top = fields.Float(string='Position top')
-
+    surcharge = fields.Float(string='Surcharge')
     stitch = fields.Integer(string='Stitch', default=0)
+    surcharge_description = fields.Char(string='Surcharge Desc')
+
+    image = fields.Binary('Image', attachment=True)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    order_garment = fields.One2many('sale.order.garment', 'order_id', string='Order Garments', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True, auto_join=True)
+    order_garment = fields.One2many('sale.order.garment.info', 'order_id', string='Order Garments', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True, auto_join=True)
 
     buyer_name = fields.Char(string='Buyer')
     po_number  = fields.Char(string='P.O#')
@@ -171,6 +162,30 @@ class SaleOrderLine(models.Model):
     
     surcharge = fields.Float(string='Surcharge', default=0)
 
-    # top bottom left right front back
-    garment_ids = fields.Many2many(
-        'sale.order.garment','sale_order_line_garment_rel','sale_order_line_id','garment_id', string='Garments', required=True)
+    logo_name = fields.Char(string='Design Name', store=False, compute='_get_product_name')
+    logo_service = fields.Char(string='Service', store=False, compute='_get_logo_service')
+    service_type = fields.Char(string='Type', store=False, compute='_get_service_type')
+    logo_stitch = fields.Integer(string='Ks', store=False, compute='_get_logo_stitch')
+
+    @api.multi
+    def _get_logo_stitch(self):
+        for ol in self:
+            logo = self.env['sale.order.logo'].browse(ol.product_id.logo_id.ids)
+            ol.logo_stitch = logo.stitch
+
+    @api.multi
+    def _get_service_type(self):
+        for ol in self:
+            ol.service_type = ol.product_id.product_type
+
+    @api.multi
+    def _get_logo_service(self):
+        for ol in self:
+            logo = self.env['sale.order.logo'].browse(ol.product_id.logo_id.ids)
+            ol.logo_service = logo.service               
+
+    @api.multi
+    def _get_product_name(self):
+        for ol in self:
+            logo = self.env['sale.order.logo'].browse(ol.product_id.logo_id.ids)
+            ol.logo_name = logo.name         
